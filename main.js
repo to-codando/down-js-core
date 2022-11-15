151f56588c0a2ac6ebc426f7bfff53576c51df52
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2022-11-15 01:18:50 -0300

    feat: apply visual and logical partial behavior

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ import { componentFactory } from './lib/index.js'
 import { header } from './components/appHeader.js'
 import { menu } from './components/appMenu.js'
 
-import { input, inputState, inputWatcher } from './components/d-input/index.js'
+import {
+  input,
+  inputState,
+  inputWatcher
+} from './components/atoms/d-input/index.js'
 
 // const appMenu = componentFactory()
 
 const appInput = componentFactory(input)
 appInput.register(document.querySelector('app-input'))
-// appHeader.setProps({ label: 'Um subtítulo para o component' })
+appInput.setProps({ label: 'Nome:' })
 // appHeader.setChildren('app-logo', menu)
 appInput.init()
 
+inputState.set({ value: 'Rodrigo Rocha' })
 inputState.on((data) => console.log('Novo valor: ', data))
 
 // console.log(appHeader)
