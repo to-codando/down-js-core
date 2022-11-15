8ca5e1aefa729fa1696b30a09b5107f39cad8225
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2022-11-14 21:13:30 -0300

    feat: create documentation app

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ import { componentFactory } from './lib/index.js'
 import { header } from './components/appHeader.js'
 import { menu } from './components/appMenu.js'
 
+import { input, inputState, inputWatcher } from './components/d-input/index.js'
+
 // const appMenu = componentFactory()
 
-const appHeader = componentFactory(header)
-appHeader.register(document.querySelector('[component=app-header]'))
-appHeader.setProps({ label: 'Um subtítulo para o component' })
-appHeader.setChildren('app-logo', menu)
-appHeader.init()
+const appInput = componentFactory(input)
+appInput.register(document.querySelector('app-input'))
+// appHeader.setProps({ label: 'Um subtítulo para o component' })
+// appHeader.setChildren('app-logo', menu)
+appInput.init()
+
+inputState.on((data) => console.log('Novo valor: ', data))
 
 // console.log(appHeader)
