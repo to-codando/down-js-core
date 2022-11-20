972c28e2f2c7a626c36bba19d147e4bab9db182d
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2022-11-20 18:26:06 -0300

    feature: include index.d.ts and increment version

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export { dataBind } from "./src/dataBind.factory.js";
+export { observerFactory } from "./src/observer.factory.js";
+export { pubsubFactory } from "./src/pubsub.factory.js";
+export { storeFactory } from "./src/store.factory.js";
+export namespace dawnJS {
+    namespace component {
+        export { componentFactory as create };
+    }
+}
+import { componentFactory } from "./src/component.factory.js";
+export { required, isEmail, isPhone, isUF, isCEP } from "./src/formValidators/index.js";
