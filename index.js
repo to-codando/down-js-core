dae70a162fdcbdfbfb5ffcc95f3f218d5c0a4eca
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2022-11-20 12:14:14 -0300

    feat: Include lib resources exportation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { componentFactory } from './src/component.factory.js'
 export { required, isEmail, isPhone, isUF, isCEP } from './src/validators.js'
+export { dataBind } from './src/dataBind.factory.js'
+export { observerFactory } from './src/observer.factory.js'
+export { pubsubFactory } from './src/pubsub.factory.js'
+export { storeFactory } from './src/store.factory.js'
 
 export const dawnJS = {
   component: { create: componentFactory }
